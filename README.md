53ab468f99881b3f08445c4d23c88a4de4a2cba6
kimisc <kimarp15@gmail.com>
2023-07-02 15:03:49 +0300

    add cert manager and ksops

--- README.md
+++ README.md
@@ -1,12 +1,25 @@
 ## Install
+
+Add age keys to `~/.config/sops/age/keys.txt`
+
 Run `install.sh` (read it first, it overwrites kubeconfig)
 
 Get initial admin secret
 
 `kubectl get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" -n argocd | base64 -d`
 
+## Adding secrets
+
+Encrypt the secret using sops
+
+`sops -e my-resource.yaml > k8s-home/secrets/my-resource.enc.yaml`
+
+
 ### Misc commands
 
 Force ArgoCD resources to be deleted if they get stuck
 `kubectl patch crd CRD_NAME -p '{"metadata": {"finalizers": null}}' --type merge`
+
+Restart argocd server
+`kubectl --namespace argocd rollout restart deployment argo-cd-argocd-server`
 
--- install.sh
+++ install.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
+set -e
+
 cp ~/.kube/config ~/.kube/config_bak
 
+echo "install k3s.."
 curl -sfL https://get.k3s.io | sh -s - --node-name=homeserver.internal --disable=traefik && sudo cat /etc/rancher/k3s/k3s.yaml >> ~/.kube/config
 
-helm install argo-cd gitops/argo-cd/ --namespace=argocd --create-namespace   && helm template -n argocd gitops/apps | kubectl -n argocd apply -f -
+# Required for secrets
+mkdir plugins/
+echo "install ksops..."
+curl -s -L https://github.com/viaduct-ai/kustomize-sops/releases/download/v4.2.1/ksops_4.2.1_Linux_x86_64.tar.gz | tar -xz -C ./plugins
+sudo mv ./plugins/ksops /usr/local/bin
+
+# ksops dependency
+echo "install kustomize..."
+curl -s -L https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv5.1.0/kustomize_v5.1.0_linux_amd64.tar.gz | tar -xz -C ./plugins
+sudo mv ./plugins/kustomize /usr/local/bin
+rm -rf ./plugins
+
+echo "install argocd helm chart..."
+helm install argo-cd gitops/argo-cd/ --namespace=argocd --create-namespace && helm template -n argocd gitops/apps | kubectl -n argocd apply -f -
+
+# Create age keys secrets for ksops to decrypt
+cat ~/.config/sops/age/keys.txt | kubectl create secret generic sops-age --namespace=argocd \
+--from-file=keys.txt=/dev/stdin -n argocd
+
