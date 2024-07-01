b27089ae5cceb05894f202b39a11783e323e5f11
kimisc <kimarp15@gmail.com>
2024-07-01 21:28:21 +0300

    add static password

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ set -e
 
 cp ~/.kube/config ~/.kube/config_bak
 
+echo "install apache2-utils for setting argocd password.."
+sudo apt install apache2-utils
+
 echo "install k3s.."
 curl -sfL https://get.k3s.io | sh -s - --node-name=homeserver.internal --disable=traefik && sudo cat /etc/rancher/k3s/k3s.yaml >> ~/.kube/config
 
@@ -21,6 +24,10 @@ rm -rf ./plugins
 
 echo "install argocd helm chart..."
 helm install argo-cd gitops/argo-cd/ --namespace=argocd --create-namespace && helm template -n argocd gitops/apps | kubectl -n argocd apply -f -
+
+echo "change admin password..."
+kubectl -n argocd patch secret argocd-secret -p '{"stringData": {"admin.password": "'$(htpasswd -bnBC 10 "" $(sops -d sops_argocd.enc.txt) | tr -d ':\n' | sed 's/$2y/$2a/')'", "admin.passwordMtime": "'$(date +%FT%T%Z)'"}}'
+
 
 # Create age keys secrets for ksops to decrypt
 cat ~/.config/sops/age/keys.txt | kubectl create secret generic sops-age --namespace=argocd \
